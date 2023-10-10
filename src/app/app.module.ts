9aa1c089c7bcd446b87ae657bf806272468731c8
Oneshot369 <joshcpeck4@gmail.com>
2023-10-10 11:08:40 -0700

    setting up angular starting project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeaturesComponent } from './features/features.component';
+import { HomeComponent } from './home/home.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeaturesComponent,
+    HomeComponent,
+    PricingComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
